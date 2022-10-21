4c4c3fbfc38d9eb6f94f3dcbdbd3738c5c6b6ae3
JaeyoonCheon <cjswodbs50@gmail.com>
2022-10-21 18:27:58 +0900

    FEAT / <RegisterPage /> 1. 회원가입 입력 폼에 대한 유효성 검사 로직 추가. 2. 각각의 유효성 검사는 백엔드단의 검증이 아닌 입력 형식 선에서 정규식과의 대조 방식으로 작동됨.

--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -59,6 +59,18 @@ const RegisterPage = () => {
   const [passwordConfirm, setPasswordConfirm] = useState("");
   const [phoneNumber, setPhoneNumber] = useState("");
 
+  const [usernameMessage, setUsernameMessage] = useState("");
+  const [emailMessage, setEmailMessage] = useState("");
+  const [passwordMessage, setPasswordMessage] = useState("");
+  const [passwordConfirmMessage, setPasswordConfirmMessage] = useState("");
+  const [phoneNumberMessage, setPhoneNumberMessage] = useState("");
+
+  const [validUsername, setValidUsername] = useState(false);
+  const [validemail, setValidEmail] = useState(false);
+  const [validpassword, setValidPassword] = useState(false);
+  const [validpasswordConfirm, setValidPasswordConfirm] = useState(false);
+  const [validphoneNumber, setValidPhoneNumber] = useState(false);
+
   const onSubmit = useCallback(
     async (e) => {
       e.preventDefault();
@@ -84,20 +96,70 @@ const RegisterPage = () => {
     [email, password, username, phoneNumber]
   );
 
-  const onChangeUsername = (e) => {
+  const onChangeUsername = useCallback((e) => {
+    const regex = /^[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]$/;
     setUsername(e.target.value);
-  };
+
+    if (regex.test(e.target.value)) {
+      if (e.target.value.length >= 2 && e.target.value.length <= 10) {
+        setValidUsername(true);
+        setUsernameMessage("");
+      } else {
+        setValidUsername(false);
+        setUsernameMessage("2자 이상 10자 이하로 입력해 주세요");
+      }
+    } else {
+      setValidUsername(false);
+      setUsernameMessage("이름은 한글로 입력되어야 합니다");
+    }
+  }, []);
+
   const onChangeEmail = (e) => {
+    const regex =
+      /^([0-9a-zA-Z_\.-]+)@([0-9a-zA-Z_-]+)(\.[0-9a-zA-Z_-]+){1,2}$/;
     setEmail(e.target.value);
+
+    if (regex.test(e.target.value)) {
+      setValidEmail(true);
+      setEmailMessage("");
+    } else {
+      setValidEmail(false);
+      setEmailMessage("올바르지 않은 이메일 형식입니다");
+    }
   };
+
   const onChangePassword = (e) => {
+    const regex = /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,30}$/;
     setPassword(e.target.value);
+
+    if (regex.test(e.target.value)) {
+      setValidEmail(true);
+      setPasswordMessage("");
+    } else {
+      setValidEmail(false);
+      setPasswordMessage(
+        "비밀번호는 8~30자리의 숫자, 영문자, 특수문자 조합으로 구성되어야 합니다"
+      );
+    }
   };
   const onChangePasswordConfirm = (e) => {
     setPasswordConfirm(e.target.value);
+
+    if (e.target.value === password) {
+      setValidPasswordConfirm(true);
+      setPasswordConfirmMessage("");
+    } else {
+      setValidPasswordConfirm(false);
+      setPasswordConfirmMessage("비밀번호가 일치하지 않습니다");
+    }
   };
   const onChangePhoneNumber = (e) => {
-    setPhoneNumber(e.target.value);
+    const autoHyphen = (number) => {
+      return number
+        .replace(/[^0-9]/g, "")
+        .replace(/^(\d{2,3})(\d{3,4})(\d{4})$/, `$1-$2-$3`);
+    };
+    setPhoneNumber(autoHyphen(e.target.value));
   };
 
   return (
@@ -110,11 +172,13 @@ const RegisterPage = () => {
             value={username}
             onChange={onChangeUsername}
           ></Input>
+          {!validUsername && <div>{usernameMessage}</div>}
           <Input
             placeholder="이메일"
             value={email}
             onChange={onChangeEmail}
           ></Input>
+          {!validemail && <div>{emailMessage}</div>}
           <div className="redundancyCheck">
             <SmallButton backgroundColor={colors.blue[0]}>중복체크</SmallButton>
           </div>
@@ -123,16 +187,19 @@ const RegisterPage = () => {
             value={password}
             onChange={onChangePassword}
           ></Input>
+          {!validpassword && <div>{passwordMessage}</div>}
           <Input
             placeholder="비밀번호 확인"
             value={passwordConfirm}
             onChange={onChangePasswordConfirm}
           ></Input>
+          {!validpasswordConfirm && <div>{passwordConfirmMessage}</div>}
           <Input
-            placeholder="전화번호"
+            placeholder="휴대폰 번호"
             value={phoneNumber}
             onChange={onChangePhoneNumber}
           ></Input>
+          {!validphoneNumber && <div>{phoneNumberMessage}</div>}
           <div className="confirmButton">
             <LargeButton backgroundColor={colors.blue[0]}>회원가입</LargeButton>
           </div>
